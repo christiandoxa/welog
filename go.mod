fef607136ccae90f3005f463ec69461f6edf11f1
Christian Doxa Hamasiah <21328162+christiandoxa@users.noreply.github.com>
2024-09-27 10:33:33 +0700

    Merge pull request #14
    * added welog config

--- go.mod
+++ go.mod
@@ -8,7 +8,6 @@ require (
 	github.com/goccy/go-json v0.10.3
 	github.com/gofiber/fiber/v2 v2.52.5
 	github.com/google/uuid v1.6.0
-	github.com/joho/godotenv v1.5.1
 	github.com/sirupsen/logrus v1.9.3
 	github.com/stretchr/testify v1.9.0
 	github.com/valyala/fasthttp v1.56.0
--- pkg/constant/envkey/envkey.go
+++ pkg/constant/envkey/envkey.go
@@ -6,16 +6,16 @@ package envkey
 
 // ElasticIndex is the environment variable key used to specify the index name for ElasticSearch.
 // This index is used to store logs and other structured data within the ElasticSearch cluster.
-const ElasticIndex = "ELASTIC_INDEX"
+const ElasticIndex = "ELASTIC_INDEX__"
 
 // ElasticPd is the environment variable key used to specify the password for authenticating
 // with ElasticSearch. This password, together with the username, secures the connection to ElasticSearch.
-const ElasticPd = "ELASTIC_PASSWORD"
+const ElasticPd = "ELASTIC_PASSWORD__"
 
 // ElasticURL is the environment variable key used to specify the URL of the ElasticSearch instance.
 // This URL is required to connect the application to the ElasticSearch service for logging and data storage.
-const ElasticURL = "ELASTIC_URL"
+const ElasticURL = "ELASTIC_URL__"
 
 // ElasticUsername is the environment variable key used to specify the username for authenticating
 // with ElasticSearch. This username, in combination with the password, provides secure access to ElasticSearch.
-const ElasticUsername = "ELASTIC_USERNAME"
+const ElasticUsername = "ELASTIC_USERNAME__"
